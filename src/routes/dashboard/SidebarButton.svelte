63e9eb44461470624f004365d87c5a23b3ba393a
BUBBLe4878 <steveawesome4878@gmail.com>
2026-04-19 12:41:16 -0500

    Update SidebarButton.svelte

--- src/routes/dashboard/SidebarButton.svelte
+++ src/routes/dashboard/SidebarButton.svelte
@@ -13,7 +13,7 @@
 
 <a
 	href={isExactMatch ? null : href}
-	class={`pei-button flex h-12 items-center rounded-lg justify-center gap-1.5 shadow-xl/3 transition-colors hover:outline-primary-100 2xl:h-13 ${pei ? `pei${pei}` : ''} ${
+	class={`pei-button pei1 flex h-12 items-center rounded-lg justify-center gap-1.5 shadow-xl/3 transition-colors hover:outline-primary-100 2xl:h-13 ${pei ? `pei${pei}` : ''} ${
 		admin
 			? (isCurrentPage ? 'bg-primary-700 border-2 border-black' : 'bg-primary-800') + ' hover:bg-primary-700 border-2 border-dotted border-white'
 			: (isCurrentPage ? 'bg-primary-700 border-2 border-black' : 'bg-primary-800') + ' hover:bg-primary-700'
