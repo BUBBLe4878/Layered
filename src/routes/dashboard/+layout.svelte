121f9f029d2aa4e9e3b4dc02b5088abc5e584e83
BUBBLe4878 <steveawesome4878@gmail.com>
2026-04-19 15:07:32 -0500

    Update +layout.svelte

--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -1,14 +1,12 @@
 <script lang="ts">
-	import Sidebar from './Sidebar.svelte';
+	import Header from './Header.svelte';
 	import { ProgressBar } from "@prgm/sveltekit-progress-bar";
-
 	let { children, data } = $props();
 </script>
 
 <ProgressBar class="text-primary-500" />
-
-<div class="box-border flex h-[100svh] flex-row">
-	<Sidebar user={data.user} />
+<Header user={data.user} />
+<div class="flex flex-col h-[calc(100svh-60px)]">
 	<div class="grow overflow-auto px-5 pt-5">
 		{@render children?.()}
 	</div>
