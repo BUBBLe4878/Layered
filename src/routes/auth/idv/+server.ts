d2cdbba6bf80c08d7ded6474319df6f91787c179
BUBBLe4878 <steveawesome4878@gmail.com>
2026-04-20 14:22:55 -0500

    Update +server.ts

--- src/routes/auth/idv/+server.ts
+++ src/routes/auth/idv/+server.ts
@@ -1,20 +1,15 @@
 import { env } from '$env/dynamic/private';
 import { redirect } from '@sveltejs/kit';
 // import crypto from 'crypto';
-
 export function GET({ url }) {
 	// const state = crypto.randomBytes(32).toString('hex');
 	// cookies.set('oauth_state', state, { path: '/', maxAge: 600 });
-	const scopes = env.AIRTABLE_TOKEN
-		? 'slack_id basic_info address'
-		: 'name slack_id verification_status';
-
+	const scopes = 'openid profile email phone address birthdate';
 	const redirectURL = new URL(`https://${env.IDV_DOMAIN}/oauth/authorize`);
 	redirectURL.searchParams.set('client_id', env.IDV_CLIENT_ID ?? '');
 	redirectURL.searchParams.set('redirect_uri', `${url.protocol}//${url.host}/auth/callback`);
 	redirectURL.searchParams.set('response_type', 'code');
 	redirectURL.searchParams.set('scope', scopes);
 	// redirectURL.searchParams.set('state', state);
-
 	return redirect(302, redirectURL);
 }
