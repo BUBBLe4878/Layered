87a34907c13c4318777dbde8c15b6a54e9edd246
BUBBLe4878 <steveawesome4878@gmail.com>
2026-04-21 08:35:38 -0500

    Update +error.svelte

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -6,6 +6,6 @@
 	<p>oh no!!!</p>
 	<h1 class="font-hero text-2xl">Error {page.status}</h1>
 	<p class="text-lg font-medium">{page.error?.message}</p>
-	<img src="https://slack-files.com/T09V59WQY1E-F0ATRAQ9PRV-ecd87f9b7d" class="mt-2" alt={`error ${page.status}`} />
+	<img src="https://slack-files.com/T09V59WQY1E-F0AV4PSNX6C-457da4921f" class="mt-2" alt={`error ${page.status}`} />
 	<a href="/" class="underline mt-10">Go back home</a>
 </div>
