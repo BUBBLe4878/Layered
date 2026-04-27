ca587b5416259f4d465aee6b9fe2b0a12f66c4e2
BUBBLe4878 <steveawesome4878@gmail.com>
2026-04-27 08:54:33 -0500

    Update +page.svelte

--- src/routes/dashboard/market/hidden/+page.svelte
+++ src/routes/dashboard/market/hidden/+page.svelte
@@ -5,26 +5,11 @@
 	import MarketTimer from './MarketTimer.svelte';
 	let { data } = $props();
 </script>
-<Head title="Printshop" />
+<Head title="Hidden Market" />
 <h1 class="mt-5 mb-2 font-hero text-3xl font-medium">Printshop</h1>
 {#if data.marketItems.length === 0}
 	<MarketTimer />
 {:else}
-	<p class="mb-2">
-		Experience: <span class="rounded-xl bg-primary-800 px-2"
-			>{Math.floor(data.user.shopScore)}</span
-		>
-		<span class="opacity-50">(allows you to get stuff for cheaper and unlock more items!)</span>
-	</p>
-	<a href={resolve('/dashboard/market/printer')} class="button md primary mb-5">Printshop printers</a>
-	
-	<!-- Hidden button that only shows when user has a printer -->
-	{#if data.user.hasBasePrinter}
-		<a href={resolve('/dashboard/market/hidden')} class="button md secondary mb-5">
-			Hidden Market
-		</a>
-	{/if}
-	
 	<h2 class="mb-2 text-2xl font-bold">Printshop items</h2>
 	<div class="mb-5 grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3">
 		{#each data.marketItems as item (item.id)}
