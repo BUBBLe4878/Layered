d2f1ad05f81da4cb5987638186217e3386ea47ef
BUBBLe4878 <steveawesome4878@gmail.com>
2026-05-01 13:16:21 -0500

    Update +page.svelte

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import '../styles/global.css';
 	import Head from '$lib/components/Head.svelte';
 	import ChecklistItem from '$lib/components/ChecklistItem.svelte';
 	import { BarChart3, BookOpen, Compass, PencilRuler, Store, Moon, Sun, Sparkles, Zap, Target, Flame } from '@lucide/svelte';
